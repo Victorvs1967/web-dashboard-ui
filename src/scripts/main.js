1813cca823c50a6532a43ebbc7088accd6a2979f
Victorvs1967 <victorsmirnov@me.com>
2023-05-24 21:36:59 +0300

    create application

---
+++ src/scripts/main.js
@@ -0,0 +1,16 @@
+// import { burgerMenu } from './include/navbar.js';
+// import { sendMsg, gallery, carousell } from "./include/func.js";
+
+// send email from formspree.io service with ajax
+// sendMsg();
+
+// picture gallery with Fancybox framework
+// gallery();
+
+// slider with Fancybox framework
+// carousell();
+
+// burgerMenu();
+
+
+// Nope. This is a concept design, so no menu or filter toggles work. But it looks good, doesn't it?
